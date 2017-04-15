a201be77ca38e064dd9f46b6f165c14cd27da68c
jagadeesha <jagadeesha.kan@gmail.com>
2017-04-15 15:00:24 +0530

    ...

--- vatic-install.sh
+++ vatic-install.sh
@@ -1,10 +1,12 @@
-sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.1 mysql-client-5.1 libmysqlclient-dev gfortran 
+#@IgnoreInspection BashAddShebang
 
+sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi libmysqlclient-dev gfortran
+#sudo apt install mysql-server-5.1 mysql-client-5.1
 sudo easy_install -U SQLAlchemy wsgilog pil cython mysql-python munkres parsedatetime argparse
 sudo easy_install -U numpy
 
 git clone https://github.com/cvondrick/turkic.git
-git clone https://github.com/cvondrick/pyvision.git
+git clone https://github.com/kanihal/pyvision.git
 git clone https://github.com/cvondrick/vatic.git
 
 cd turkic
